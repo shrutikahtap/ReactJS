6bebfa7f38b7b5d847506eba12a8cdc435d4ed7c
shruti <shrutipathak1201@gmail>
2026-04-11 18:05:55 +0530

    Intro To Frontend

---
+++ app.js
@@ -0,0 +1,4 @@
+var hi = "Good morning";
+// console.log(hi);
+
+export default hi;
---
+++ script.js
@@ -0,0 +1,17 @@
+var h1=document.createElement('h1');
+h1.textContent = 'Namaste';
+h1.style.color = 'red';
+document.body.appendChild(h1);
+console.log(h1);
+
+const a = 5;
+console.log(a);
+
+import hi from './app.js';
+console.log(hi);
+
+var h2=document.createElement('div');
+h2.textContent = 'Radhe Radhe';
+// h2.style.color = 'blue';
+// document.body.appendChild(h2);
+console.log(h2);
