6a86103f5c057d6f732e922d1ec34b9c43a0f9ca
shruti <shrutipathak1201@gmail>
2026-04-12 11:39:56 +0530

    A little updates

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var hi = "Good morning";
-// console.log(hi);
-
 export default hi;
+
+const arr= [1,2,3,4,5];
+export {arr};
+
+export var me = "Hello future me !!!";
--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var h1=document.createElement('h1');
-h1.textContent = 'Namaste';
-h1.style.color = 'red';
+h1.textContent = 'Radhe Radhe';
+h1.style.color = 'blue';
 document.body.appendChild(h1);
 console.log(h1);
+
+var h2=document.createElement('div');
+h2.textContent = 'Namaste Duniya';
+h2.style.font = 'italic bold 20px arial,serif';
+h2.style.color = 'yellow';
+document.body.appendChild(h2);
+console.log(h2);
 
 const a = 5;
 console.log(a);
@@ -10,8 +17,8 @@ console.log(a);
 import milton from './app.js';
 console.log(milton);
 
-var h2=document.createElement('div');
-h2.textContent = 'Radhe Radhe';
-// h2.style.color = 'blue';
-// document.body.appendChild(h2);
-console.log(h2);
+import {arr} from './app.js';
+console.log(arr);
+
+import {me} from './app.js';
+console.log(me);
