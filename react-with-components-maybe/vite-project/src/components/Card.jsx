c090c7ed32be74edaa0bde04bce4c5d596eebb27
shruti <shrutipathak1201@gmail>
2026-04-13 23:01:38 +0530

    Update

---
+++ react-with-components-maybe/vite-project/src/components/Card.jsx
@@ -0,0 +1,10 @@
+function card() {
+  return (
+    <div className="card">
+      <h2>SHRUTI PATHAK</h2>
+      <p>GOD'S FAVOURITE CHILD !!!</p>
+    </div>
+  );
+}
+
+export default card;
---
+++ react-with-components-maybe/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>My Website</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
